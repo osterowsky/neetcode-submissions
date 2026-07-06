7b524bb87c9c814c900e5942e0ce1a15b39eec3a
Patryk Ostern <105588004+osterowsky@users.noreply.github.com>
2026-07-06 13:39:32 +0200

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        
+        first, second = 1, 2
+        for i in range(3, n + 1):
+            temp = second
+            second = second + first
+            first = second
+
+        return second
